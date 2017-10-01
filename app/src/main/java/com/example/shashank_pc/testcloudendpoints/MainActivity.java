8efb1bcf8faca9f02424a09692e71c07010bc526
Shashank Anantharam <shashank95.a@gmail.com>
2017-10-01 22:37:31 +0530

    Small breakthrough

--- app/src/main/java/com/example/shashank_pc/testcloudendpoints/MainActivity.java
+++ app/src/main/java/com/example/shashank_pc/testcloudendpoints/MainActivity.java
@@ -4,8 +4,15 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.shashank_pc.myapplication.backend.TempClassEndpoint;
 import com.example.shashank_pc.myapplication.backend.tempClassApi.TempClassApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.appengine.repackaged.com.google.api.client.json.jackson2.JacksonFactory;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +30,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            TempClassApi.Builder = new TempClassApi.Builder(
-                    AndroidHttp
-            )
+            TempClassApi.Builder builder = new TempClassApi.Builder(
+                    AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setHttpRequestInitializer(new HttpRequestInitializer() {
+                        @Override
+                        public void initialize(HttpRequest request) throws IOException {
+
+                        }
+                    });
+
+
+
 
             return null;
         }
