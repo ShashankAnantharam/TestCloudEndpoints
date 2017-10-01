68c3019e75251128194239743cc431a5ea64b73a
Shashank Anantharam <shashank95.a@gmail.com>
2017-10-01 22:21:48 +0530

    Changed

--- app/src/main/java/com/example/shashank_pc/testcloudendpoints/MainActivity.java
+++ app/src/main/java/com/example/shashank_pc/testcloudendpoints/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shashank_pc.testcloudendpoints;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    private class Testtask extends AsyncTask<String,String,String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            
+            return null;
+        }
     }
 }
